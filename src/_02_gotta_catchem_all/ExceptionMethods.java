908436c108788cb0de68636ea2276d2243365b65
brandonzaayer <33267170+brandonzaayer@users.noreply.github.com>
2021-10-03 12:57:24 -0700

    exceptions

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,15 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public double divide(double a, double b) throws IllegalArgumentException{
+		double c = a/b;
+		if(b == 0.0) {
+			throw new IllegalArgumentException();
+		}
+		return c;
+	}
+	public String reverseString(String s) throws IllegalStateException{
+		
+		return null;
+	}
 }
